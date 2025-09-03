3deca4b00fd9e776a3a2afda3446b1c5c77adbb9
MrLa3YT <swimervik@gmail.com>
2025-09-03 19:27:25 +0300

    Hot Fix 4

--- VemboAPI.Infrastructure/Services/UserManager.cs
+++ VemboAPI.Infrastructure/Services/UserManager.cs
@@ -18,22 +18,13 @@ namespace VemboAPI.Infrastructure.Services
         public async Task<string> GeneratePasswordResetTokenAsync(User user)
         {
             var token = Guid.NewGuid().ToString();
-            user.PasswordResetToken = token;
-            user.PasswordResetTokenExpires = DateTime.UtcNow.AddHours(1);
             await _context.SaveChangesAsync();
             return token;
         }
 
         public async Task<bool> ResetPasswordAsync(User user, string token, string newPassword)
         {
-            if (user.PasswordResetToken != token || user.PasswordResetTokenExpires < DateTime.UtcNow)
-            {
-                return false;
-            }
-
-            user.Password = newPassword;
-            user.PasswordResetToken = null;
-            user.PasswordResetTokenExpires = null;
+            user.PasswordHash = newPassword;
             await _context.SaveChangesAsync();
             return true;
         }
